30d155dbae625ca7ef6ac44eefb9cb770687fd76
mimamuAO3 <82106526+mimamuAO3@users.noreply.github.com>
2021-04-09 13:32:36 +0300

    Update ficcikone.js

--- ficcikone.js
+++ ficcikone.js
@@ -1,5 +1,14 @@
 function listaus() {
 	if (document.getElementById("fandom").value == 1) {
-		document.getElementById("listaus").innerHTML = "1";
+		document.getElementById("listaus").innerHTML = "Potter";
+	}
+	else if {
+		document.getElementById("listaus").innerHTML = "SW";
+	}
+	else if {
+		document.getElementById("listaus").innerHTML = "rare";
+	}
+	else {
+		document.getElementById("listaus").innerHTML = "";
 	}
 }
